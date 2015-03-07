13c95db975fe1924ba77570e81aa1ac9b3348aae
DmitryAB <arbaitenmail@gmail.com>
2015-03-07 11:16:25 -0800

    added mongodb storage

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ global.__basedir = __dirname;
 var express = require('express');
 var bodyParser = require('body-parser');
 var fs = require('fs');
+var mongoose = require('mongoose');
+var Tree = mongoose.model('Tree', { tree: String });
+
+mongoose.connect('mongodb://localhost/trees');
 
 app = express();
 app.use(bodyParser.json());
@@ -12,20 +16,27 @@ app.set('view engine', 'jade');
 app.use(express.static(__basedir + '/public'));
 
 app.get('/',function(req, res){
-  res.render('index', {
-     title: "Tree",
-     tree: JSON.parse(fs.readFileSync(__basedir + '/tree.json', 'utf8'))
+
+  Tree.findOne({},function(err,docs){
+    res.render('index',{
+      title: "Tree",
+      tree: JSON.parse(docs ? docs.tree : "{\"name\":\"Древо\",\"nodes\":[]}")
+    });
   });
+
 });
 
 app.post('/',function(req, res){
-  
-  fs.writeFile(__basedir + '/tree.json', JSON.stringify(req.body), function(err) {
-      if(err) {
-          console.log(err);
-      } else {
-          res.end("Saved!");
-      }
+    
+  Tree.remove({}, function(err) { 
+    console.log('collection removed') 
+  });
+  var tree = new Tree({ tree: JSON.stringify(req.body) });
+  tree.save(function (err) {
+    if (err) 
+      console.log(err);
+    else
+      res.end("Saved!");
   });
 });
 
